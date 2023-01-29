dd913b07dff72a5e69fd422730be1307e0562b19
Sabta Mendes <sabtaamendes@gmail.com>
2023-01-28 20:39:23 -0400

    feat: all tests passed

--- src/schemas/payments-schemas.ts
+++ src/schemas/payments-schemas.ts
@@ -1,15 +1,13 @@
 import {  Payments } from "@/protocols";
 import Joi from "joi";
 
-const regex = /((0[1-9])|(1[0-2]))\/(\d{4})/;
-
 export const paymentsSchema = Joi.object<Payments>({
   ticketId: Joi.number().min(1).integer().required(),
   cardData: Joi.object({
-    issuer: Joi.string().valid("VISA", "MASTERCARD").required(),
+    issuer: Joi.string().required(),
     number: Joi.number().required(),
     name: Joi.string().required(),
-    expirationDate: Joi.string().regex(regex).required(),
+    expirationDate: Joi.string().required(),
     cvv: Joi.number().required()
   })
 });
